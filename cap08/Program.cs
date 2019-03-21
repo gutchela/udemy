6142e66ce006559d42284f4f267855c50c3d689e
Gustavo Duarte <gustavodo2011@gmail.com>
2019-03-21 08:04:07 -0300

    Projeto iniciado, mostrando mensagem

---
+++ cap08/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cap08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
